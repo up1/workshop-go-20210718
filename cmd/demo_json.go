15384281242c55e89a9da26e073c0e0fa5dea4fc
up1 <somkiat.p@gmail.com>
2021-07-18 14:37:29 +0700

    Working json

---
+++ cmd/demo_json.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type User struct {
+	Id int	`json:"id"`  //json literal
+	Name string `json:"name"`
+}
+
+func main(){
+	// Struct to JSON
+	u := User{Id: 1, Name: "Somkiat"}
+	e := json.NewEncoder(os.Stdout)
+	e.Encode(u)
+
+	// JSON to Struct
+	const jsonStream = `
+	{"id": 1, "name": "Somkiat"}`
+
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		var m User
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v: %v\n", m.Id, m.Name)
+	}
+}
