e4bca1b5121684edb0b5ed8da79126ed80166d4f
up1 <somkiat.p@gmail.com>
2021-07-18 13:32:50 +0700

    Demo with struct

--- cmd/demo_struct.go
+++ cmd/demo_struct.go
@@ -2,14 +2,24 @@ package main
 
 import "fmt"
 
-
+type department struct {
+	depName string
+}
 type person struct {
 	id int
 	name string
+	department // Embedded struct
 }
 
+func (p person) doSth() {
+	p.name = "Update name"
+	fmt.Println("Called doSth() with name=", p.name)
+}
 
 func main() {
-	p := person{id: 1, name: "somkiat"}
-	fmt.Printf("%+v", p)
+    d := department{"demo"}
+	p := person{id: 1, name: "somkiat", department: d}
+	p.doSth()
+	fmt.Println(p.depName)
+	fmt.Println(p.department.depName)
 }
