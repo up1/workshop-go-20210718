a707f6cddef33b2cc5477a238e0da60f04b49a67
up1 <somkiat.p@gmail.com>
2021-07-18 14:02:14 +0700

    Demo with methos overiding

--- cmd/demo_struct.go
+++ cmd/demo_struct.go
@@ -5,7 +5,27 @@ import (
 	"fmt"
 )
 
+
+type parent struct{
+}
+
+type child struct {
+	parent
+}
+
+func (p parent) callParent() {
+	fmt.Println("callParent..")
+}
+
+// func (c child) callParent() {
+// 	c.parent.callParent() // Call from parent
+// 	fmt.Println("callParent from child..")
+// }
+
 func main() {
+	c := child{}
+	c.callParent()
+
 	p := day01.NewPerson(1, "somkiat", "demo")
 	p.DoSth()
 	fmt.Printf("%+v", p)
