172cc9871aaf25cb33b6080922f3defcc86ba54f
up1 <somkiat.p@gmail.com>
2021-07-18 11:58:51 +0700

    Switch case

--- cmd/demo_map.go
+++ cmd/demo_map.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	n := 7
+	switch n {
+		case 1,5,6,7 : fmt.Println("OK 1")
+		case 2 : fmt.Println("OK 2")
+		case 3 : fmt.Println("OK 3")
+		default : fmt.Println("default")
+	}
+
 	grades := make(map[int]string)
 	grades[100] = "A"
 	grades[101] = "B"
