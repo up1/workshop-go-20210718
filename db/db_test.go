a9c65eb44f54e9606f9e71cd9aea2520ee8e8497
up1 <somkiat.p@gmail.com>
2021-07-18 10:57:55 +0700

    Working with testing

---
+++ db/db_test.go
@@ -0,0 +1,13 @@
+package db_test
+
+import (
+	"day01/db"
+	"testing"
+)
+
+func TestSuccessCase(t *testing.T) {
+	result, _ := db.SaveData("")
+	if result != 100 {
+		t.Errorf("Expect 100 but found %v", result)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module day01
 
 go 1.16
+
+require golang.org/x/tour v0.1.0 // indirect
